991245a63f57dbfee3e2268a736204a3014bfc7b
Remi LUDER <rluder@e1r5p9.42.fr>
2016-01-12 16:08:33 +0100

    it ize zeu bordel

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rluder <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 15:57:08 by rluder            #+#    #+#             */
-/*   Updated: 2015/12/14 18:24:40 by rluder           ###   ########.fr       */
+/*   Updated: 2016/01/12 16:07:57 by rluder           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,27 +25,38 @@ static char	*ft_nofile(int const fd, char *file, char *buf)
 			file = ft_strdup(buf);
 		else
 			file = ft_strjoin(file, buf);
+		if (ret == -1)
+			return (0);
 	}
+	return (file);
+}
+
+char	*get_buff(int const fd, char *file)
+{
+	char		buf[BUFF_SIZE + 1];
+
+	if (!file)
+		file = ft_nofile(fd, file, buf);
+	else if (	*file == '\0')
+		return (0);
 	return (file);
 }
 
 int	get_next_line(int const fd, char **line)
 {
-	char			buf[BUFF_SIZE + 1];
 	static char		*file;
+	char			*test;
 	unsigned int	index;
 	unsigned int	len;
 
 	index = 0;
 	len = 0;
-//	if (BUFF_SIZE <= 0 || !line)
-//		return (write(1, "nope\n", 5));
-//		return (-1);
-	if (!file)
-		file = ft_nofile(fd, file, buf);
+	*line = ft_strnew(1);
+	file = get_buff(fd, file);
+//	write(1, "gbok\n", 5);
 	if (*file == '\0')
 	{
-		return (write(1, "-1\n", 3));
+//		return (write(1, "-1\n", 3));
 		return (-1);
 	}
 	while (file[index] != '\n' && file[index] != '\0')
@@ -53,19 +64,20 @@ int	get_next_line(int const fd, char **line)
 		len++;
 		index++;
 	}
-	line = malloc(sizeof(char) * (len + 1));
-	(*line) = ft_strsub(file, 0, len);
+//	write(1, "whok\n", 5);
+	test = ft_strsub(file, 0, len);
+	printf("%s\n", test);
+	line = &test;
+//	write(1, "subok\n", 6);
 	file = &file[len];
+//	write(1, "gbok\n", 5);
 	file++;
-//	ft_putstr((*line));
-//	write(1, "\n\n", 2);
-//	ft_putstr(file);
-	if (file[len + 1] == '\0')
+	if (EOF)
 	{
-		return (write(1, "0\n", 2));
+//		return (write(1, "0\n", 2));
 		return (0);
 	}
-	return (write(1, "1\n", 2));
+//	return (write(1, "1\n", 2));
 	return (1);
 }
 
@@ -76,11 +88,12 @@ int main(int argc, const char *argv[])
 
 	fd = open(argv[1], O_RDONLY);
 	get_next_line(fd, line);
-	get_next_line(fd, line);
-	get_next_line(fd, line);
+	printf("%s", *line);
 //	get_next_line(fd, line);
 //	get_next_line(fd, line);
-//	get_next_line(fd, line);
-//	get_next_line(fd, line);
+	//	get_next_line(fd, line);
+	//	get_next_line(fd, line);
+	//	get_next_line(fd, line);
+	//	get_next_line(fd, line);
 	return (0);
 }
